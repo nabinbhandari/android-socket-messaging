8a08193e82a62f650a789c3c22bbcafa3e9af6ec
Nabin Bhandari <bnabin51@gmail.com>
2017-10-15 13:44:38 +0545

    Initial commit - New Blank Project

---
+++ app/src/main/java/nabinbhandari/com/socketmessagingexample/MainActivity.java
@@ -0,0 +1,14 @@
+package nabinbhandari.com.socketmessagingexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
